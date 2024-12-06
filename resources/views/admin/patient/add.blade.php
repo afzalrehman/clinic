277550427920c368e1443bd2308d13adad56fbaf
Muhammad Afzal <124122144+afzalrehman@users.noreply.github.com>
2024-12-06 17:07:59 +0500

    updare

--- resources/views/admin/patient/add.blade.php
+++ resources/views/admin/patient/add.blade.php
@@ -46,9 +46,9 @@
                                     <div class="col-12 col-md-6">
                                         <div class="input-block local-forms">
                                             <label>Last Name <span class="login-danger">*</span></label>
-                                            <input class="form-control" type="text" name="username"
-                                                placeholder="Enter UserName" value="{{ old('username') }}">
-                                            @error('username')
+                                            <input class="form-control" type="text" name="lastname"
+                                                placeholder="Enter Last Name" value="{{ old('lastname') }}">
+                                            @error('lastname')
                                                 <span style="color:red;font-size: 13px">{{ $message }}</span>
                                             @enderror
                                         </div>
