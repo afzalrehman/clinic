82ece7fe9a4d6ede454534459b5cf962108e88e4
Muhammad Afzal <124122144+afzalrehman@users.noreply.github.com>
2024-12-09 16:10:26 +0500

    update

--- app/Models/MailModel.php
+++ app/Models/MailModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Auth;
 use Illuminate\Database\Eloquent\Model;
 
 class MailModel extends Model
@@ -18,7 +19,7 @@ class MailModel extends Model
 
      public static function getemail()
     {
-        $return = self::select('mails.*' , 'users.username as username' , 'users.role as userRole' , 'users.email as useremail')->join('users' , 'users.id' , 'mails.to')->where('mails.status' , 'Active')->where('mails.to', '!=', 0)->where('mails.to', '!=', 1)
+        $return = self::select('mails.*' , 'users.username as username' , 'users.role as userRole' , 'users.email as useremail')->join('users' , 'users.id' , 'mails.to')->where('mails.status' , 'Active')->where('mails.to', '!=', 0)->where('mails.to', '!=', 1)->where('mails.created_id', '=', Auth::user()->id)
             ->orderBy('mails.id', 'DESC'); 
         return $return->get();
     }
@@ -31,7 +32,7 @@ class MailModel extends Model
     
      public static function getemailtrash()
     {
-        $return = self::select('mails.*' , 'users.username as username' , 'users.role as userRole' , 'users.email as useremail')->join('users' , 'users.id' , 'mails.to')->where('mails.status' , 'In Active')->where('mails.to', '!=', 0)->where('mails.to', '!=', 1)
+        $return = self::select('mails.*' , 'users.username as username' , 'users.role as userRole' , 'users.email as useremail')->join('users' , 'users.id' , 'mails.to')->where('mails.status' , 'In Active')->where('mails.to', '!=', 0)->where('mails.to', '!=', 1)->where('mails.created_id', '=', Auth::user()->id)
             ->orderBy('mails.id', 'DESC'); 
         return $return->get();
     }
