385db06813e2e2470173c71192942316e53965cb
Muhammad Afzal <124122144+afzalrehman@users.noreply.github.com>
2024-12-10 13:00:45 +0500

    update

--- resources/views/doctor/dashboard.blade.php
+++ resources/views/doctor/dashboard.blade.php
@@ -227,19 +227,6 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-12 col-md-12  col-xl-4 d-flex">
-                            <div class="card wallet-widget">
-                                <div class="circle-bar circle-bar2">
-                                    <div class="circle-graph2" data-percent="66">
-                                        <b><img src="assets/img/icons/timer.svg" alt=""></b>
-                                    </div>
-                                </div>
-                                <div class="main-limit">
-                                    <p>Next Appointment in</p>
-                                    <h4>02h:12m</h4>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
                 <div class="col-12 col-lg-12 col-xl-5 d-flex">
