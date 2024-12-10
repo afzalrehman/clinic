ea0772b7d5b68f725a724fb4e0e83019bbf886d6
Muhammad Afzal <124122144+afzalrehman@users.noreply.github.com>
2024-12-10 14:03:05 +0500

    update

--- resources/views/doctor/body/sidebar.blade.php
+++ resources/views/doctor/body/sidebar.blade.php
@@ -8,7 +8,7 @@
                         <span>Dashboard</span></a>
                 </li>
 
-                <li class="active">
+                <li class="active bg-light">
                     <a href="{{ route('doctor.doctor') }}"><span class="menu-side"><img
                                 src="{{ asset('assets/img/icons/menu-icon-02.svg') }}" alt=""></span>
                         <span>Doctors</span></a>
