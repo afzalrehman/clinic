04583d1a81176ab5f2458f89bdebdb128bde0cdd
Muhammad Afzal <124122144+afzalrehman@users.noreply.github.com>
2024-12-21 21:05:32 +0500

    update

--- resources/views/clinic/online/appoinment.blade.php
+++ resources/views/clinic/online/appoinment.blade.php
@@ -189,7 +189,7 @@
 
                 if (department_id) {
                     $.ajax({
-                        url: 'appoinment-doctor-details' + department_id,
+                        url: 'appoinment-doctor-details/' + department_id,
                         type: 'GET',
                         success: function(data) {
                             if (data) {
