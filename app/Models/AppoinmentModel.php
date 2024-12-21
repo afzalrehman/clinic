a0da72eb3da748c2a0fe4fb1e8ec9128550e47bf
Muhammad Afzal <124122144+afzalrehman@users.noreply.github.com>
2024-12-21 15:09:27 +0500

    update

--- app/Models/AppoinmentModel.php
+++ app/Models/AppoinmentModel.php
@@ -31,12 +31,10 @@ class AppoinmentModel extends Model
             ->select(
                 'appointments.*',
                 'patient.name as patient_name',
-                'patient.lastname as patient_lastname',
                 'patient.profile_photo as patient_image',
                 'patient.mobile as patient_mobile',
                 'patient.email as patient_email',
                 'doctor.name as doctor_name',
-                'doctor.lastname as doctor_lastname',
                 'department.name as department_name'
             )
             ->join('patient', 'patient.cnic', '=', 'appointments.patient_id')
@@ -48,9 +46,7 @@ class AppoinmentModel extends Model
             $search = $request->get('search');
             $query->where(function ($q) use ($search) {
                 $q->where('patient.name', 'like', '%' . $search . '%')
-                    ->orWhere('patient.lastname', 'like', '%' . $search . '%')
                     ->orWhere('doctor.name', 'like', '%' . $search . '%')
-                    ->orWhere('doctor.lastname', 'like', '%' . $search . '%')
                     ->orWhere('patient.mobile', 'like', '%' . $search . '%')
                     ->orWhere('appointments.treatment', 'like', '%' . $search . '%')
                     ->orWhere('appointments.appointment_date', 'like', '%' . $search . '%')
