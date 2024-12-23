b5bd90a4f7bc4d95b0d01d8f5c70c743f1babfd6
Muhammad Afzal <124122144+afzalrehman@users.noreply.github.com>
2024-12-23 14:26:52 +0500

    update

--- app/Http/Controllers/Admin/AppoinmentController.php
+++ app/Http/Controllers/Admin/AppoinmentController.php
@@ -261,7 +261,7 @@ class AppoinmentController extends Controller
             'doctor_id' => 'required', // Number is required
             'department_id' => 'required|integer',
             'appointment_date' => 'required',
-            'document' => 'nullable|mimes:jpg,jpeg,png,pdf,doc,docx|max:2048',
+            'document' => 'nullable|mimes:jpg,jpeg,png,pdf,doc,docx',
         ]);
 
         // Check if the patient already exists in the patient table
