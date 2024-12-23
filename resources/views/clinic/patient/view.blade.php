8b869a82e6afadd37da1abcd4cff5508d625e57f
Muhammad Afzal <124122144+afzalrehman@users.noreply.github.com>
2024-12-23 17:11:36 +0500

    update

--- resources/views/clinic/patient/view.blade.php
+++ resources/views/clinic/patient/view.blade.php
@@ -50,29 +50,7 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div class="col-lg-4 col-md-4 d-flex align-items-center">
-                                                <div class="follow-group">
-                                                    <div class="doctor-follows">
-                                                        <h5>Followers</h5>
-                                                        <h4>850</h4>
-                                                    </div>
-                                                    <div class="doctor-follows">
-                                                        <h5>Following</h5>
-                                                        <h4>18K</h4>
-                                                    </div>
-                                                    <div class="doctor-follows">
-                                                        <h5>Posts</h5>
-                                                        <h4>250</h4>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-6 col-xl-4 d-flex align-items-center">
-                                                <div class="follow-btn-group py-3">
-                                                    <button type="submit" class="btn btn-info follow-btns">Follow</button>
-                                                    <button type="submit"
-                                                        class="btn btn-info message-btns">Message</button>
-                                                </div>
-                                            </div>
+                                          
                                         </div>
                                     </div>
                                 </div>
@@ -108,60 +86,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="doctor-personals-grp">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="heading-detail">
-                                            <h4>Skills:</h4>
-                                        </div>
-                                        <div class="skill-blk">
-                                            <div class="skill-statistics">
-                                                <div class="skills-head">
-                                                    <h5>Heart Beat</h5>
-                                                    <p>45%</p>
-                                                </div>
-                                                <div class="progress mb-0">
-                                                    <div class="progress-bar bg-operations" role="progressbar"
-                                                        style="width: 45%" aria-valuenow="45" aria-valuemin="0"
-                                                        aria-valuemax="100"></div>
-                                                </div>
-                                            </div>
-                                            <div class="skill-statistics">
-                                                <div class="skills-head">
-                                                    <h5>Haemoglobin</h5>
-                                                    <p>85%</p>
-                                                </div>
-                                                <div class="progress mb-0">
-                                                    <div class="progress-bar bg-haemoglobin" role="progressbar"
-                                                        style="width: 85%" aria-valuenow="85" aria-valuemin="0"
-                                                        aria-valuemax="100"></div>
-                                                </div>
-                                            </div>
-                                            <div class="skill-statistics">
-                                                <div class="skills-head">
-                                                    <h5>Blood Pressure </h5>
-                                                    <p>65%</p>
-                                                </div>
-                                                <div class="progress mb-0">
-                                                    <div class="progress-bar bg-statistics" role="progressbar"
-                                                        style="width: 65%" aria-valuenow="65" aria-valuemin="0"
-                                                        aria-valuemax="100"></div>
-                                                </div>
-                                            </div>
-                                            <div class="skill-statistics">
-                                                <div class="skills-head">
-                                                    <h5>Sugar </h5>
-                                                    <p>90%</p>
-                                                </div>
-                                                <div class="progress mb-0">
-                                                    <div class="progress-bar bg-visit" role="progressbar" style="width: 90%"
-                                                        aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                          
                         </div>
                         <div class="col-lg-8">
                             <div class="doctor-personals-grp">
