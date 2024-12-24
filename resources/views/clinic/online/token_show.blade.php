808544bbac6f70de0e249ff8c302cffe1b11fea9
Muhammad Afzal <124122144+afzalrehman@users.noreply.github.com>
2024-12-24 19:13:36 +0500

    updaet

---
+++ resources/views/clinic/online/token_show.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from preclinic.dreamstechnologies.com/html/template/error-500.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 17 Nov 2024 04:13:21 GMT -->
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/img/favicon.png')}}">
+    <title>Preclinic - Medical & Hospital - Bootstrap 5 Admin Template</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/plugins/fontawesome/css/fontawesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/plugins/fontawesome/css/all.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
+
+</head>
+
+<body class="error-pages">
+    <div class="main-wrapper error-wrapper">
+        <div class="error-box">
+            <img class="img-fluid" src="assets/img/error-02.png" alt="Logo">
+            <h3><img class="img-fluid mb-0" src="assets/img/icons/danger.svg" alt="Logo"> Internal Server Error</h3>
+            <p>You do not have permission to view this resource</p>
+            <a href="index-2.html" class="btn btn-primary go-home">Go to Home</a>
+        </div>
+    </div>
+    <script src="{{asset('assets/js/jquery-3.7.1.min.js')}}" type="d5a0acff7c16e90f7b194957-text/javascript"></script>
+    <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}" type="d5a0acff7c16e90f7b194957-text/javascript"></script>
+    <script src="{{asset('assets/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js')}}"
+        data-cf-settings="d5a0acff7c16e90f7b194957-|49" defer></script>
+</body>
+
+
+<!-- Mirrored from preclinic.dreamstechnologies.com/html/template/error-500.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 17 Nov 2024 04:13:22 GMT -->
+
+</html>
