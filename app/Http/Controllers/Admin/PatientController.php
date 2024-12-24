895f076fc785e5aa1f7cb0724c3a078404a85678
Muhammad Afzal <124122144+afzalrehman@users.noreply.github.com>
2024-12-24 12:26:38 +0500

    updaet

--- app/Http/Controllers/Admin/PatientController.php
+++ app/Http/Controllers/Admin/PatientController.php
@@ -174,11 +174,10 @@ class PatientController extends Controller
 
     public function getPatientDetails($id)
     {
-        $patient = PatientModel::where( 'cnic', '=',$id)->where('clinic_id', Auth::user()->clinic_id)->first(); // Assuming you have a `Patient` model
+        $patient = PatientModel::where( 'mobile', '=',$id)->where('clinic_id', Auth::user()->clinic_id)->first(); // Assuming you have a `Patient` model
         if ($patient) {
             return response()->json([
                 'name' => $patient->name,
-                'lastname' => $patient->lastname,
                 'mobile' => $patient->mobile,
                 'email' => $patient->email,
                 'gender' => $patient->gender,
