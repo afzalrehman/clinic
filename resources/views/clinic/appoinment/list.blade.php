409af93f42ffa17de24e078819cdc2aeed92af10
Muhammad Afzal <124122144+afzalrehman@users.noreply.github.com>
2024-12-24 17:18:47 +0500

    updaet

--- resources/views/clinic/appoinment/list.blade.php
+++ resources/views/clinic/appoinment/list.blade.php
@@ -96,7 +96,6 @@
                                                     </div>
                                                 </td>
 
-                                               
                                                 <td class="text-end">
                                                     <div class="dropdown dropdown-action">
                                                         <a href="#" class="action-icon dropdown-toggle"
@@ -115,7 +114,14 @@
                                                     </div>
                                                 </td>
 
-                                                <td> <button  class="custom-badge status-orange  p-2">{{$value->token}}</button></td>
+                                                <td> 
+                                                    @if($value->appointment_date == now()){
+                                                        <button title="Today Appionment"  class="custom-badge status-orange  p-2">{{$value->token}}</button>
+                                                    }
+                                                    @else{
+                                                        <button title="Upcomming Appionment"  class="custom-badge status-green  p-2">{{$value->token}}</button>
+                                                    }
+                                                </td>
                                                 <td class="profile-image"><a href="{{url('/clinic/patient?search='. $value->patient_name )}}"><img width="28"
                                                             height="28"
                                                             src="{{ $value->patient_image ? asset('upload/img/patient/'.$value->patient_image) : asset('asset/img/user.jpg') }}"
