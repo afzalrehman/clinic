2b45bb4088c73b85b8c4db4579c94d978bd79ddb
Muhammad Afzal <124122144+afzalrehman@users.noreply.github.com>
2024-12-24 18:32:56 +0500

    update

--- app/Http/Controllers/Admin/PatientController.php
+++ app/Http/Controllers/Admin/PatientController.php
@@ -90,10 +90,10 @@ class PatientController extends Controller
     {
         $data['patient'] = PatientModel::find($id);
         $data['appoinment'] = AppoinmentModel::where('patient_id' , '=' , $data['patient']->mobile)->get();
-        $data['appoinment_patient'] = PatientModel::where('mobile' , '=' , $data['appoinment']->patient_id)->first();
-        $data['appoinment_doctor'] = DoctorModel::where('mobile' , '=' , $data['appoinment']->doctor_id)->first();
-        $data['appoinment_department'] = DepartmentModel::where('id' , '=' , $data['appoinment']->department_id)->first();
-        $data['appoinment_list'] = AppoinmentModel::getappoinment($request);
+        // $data['appoinment_patient'] = PatientModel::where('mobile' , '=' , $data['appoinment']->patient_id)->first();
+        // $data['appoinment_doctor'] = DoctorModel::where('mobile' , '=' , $data['appoinment']->doctor_id)->first();
+        // $data['appoinment_department'] = DepartmentModel::where('id' , '=' , $data['appoinment']->department_id)->first();
+        // $data['appoinment_list'] = AppoinmentModel::getappoinment($request);
         return view('clinic.patient.view', $data);
     }
 
